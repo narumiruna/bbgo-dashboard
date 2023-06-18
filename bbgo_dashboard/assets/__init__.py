b93ef437e873c5fd7b31a8d23786de3523efa58b
なるみ <toucans-cutouts0f@icloud.com>
2023-06-18 13:05:18 +0800

    chore: add resources and dependencies to project
    - Add `FilesystemIOManager` resource to `bbgo_dashboard`
    - Add `kaleido` dependency to `pyproject.toml`
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- bbgo_dashboard/assets/__init__.py
+++ bbgo_dashboard/assets/__init__.py
@@ -1,4 +1,5 @@
 from dagster import Definitions
+from dagster import FilesystemIOManager
 
 from .resources import DatabaseResource
 from .trades import daily_num_trades
@@ -6,5 +7,8 @@ from .trades import trades
 
 defs = Definitions(
     assets=[trades, daily_num_trades],
-    resources={'db': DatabaseResource()},
+    resources={
+        'db': DatabaseResource(),
+        'io_manager': FilesystemIOManager(base_dir='data'),
+    },
 )

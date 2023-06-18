41160929e761719dc61ca2f3d6c78f20d233d37d
なるみ <toucans-cutouts0f@icloud.com>
2023-06-18 20:01:24 +0800

    refactor: refactor trade-related functions and update dev dependencies
    - Modify the `trades` function to use `sqlalchemy select` instead of a raw SQL query
    - Change the `daily_num_trades` function to group by `side` in addition to `D`
    - Add `jupyter` as a dev dependency in `pyproject.toml`
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- bbgo_dashboard/assets/trades.py
+++ bbgo_dashboard/assets/trades.py
@@ -2,13 +2,15 @@ import pandas as pd
 from dagster import MetadataValue
 from dagster import Output
 from dagster import asset
+from sqlalchemy import select
 
+from ..db import Trade
 from .resources import DatabaseResource
 
 
 @asset
 def trades(db: DatabaseResource) -> Output:
-    df = db.read_sql('SELECT * FROM trades', parse_dates=['traded_at'])
+    df = db.read_sql(select(Trade), parse_dates=['traded_at'])
     df.set_index('traded_at', inplace=True)
     return Output(value=df,
                   metadata={
@@ -19,9 +21,8 @@ def trades(db: DatabaseResource) -> Output:
 
 @asset
 def daily_num_trades(trades: pd.DataFrame):
-    df = trades.groupby(pd.Grouper(freq='D')).count()
-    df = df[['id']]
-    df.columns = ['num_trades']
+    df = trades.groupby([pd.Grouper(freq='D'), 'side']).count()
+    df = df[['gid']]
     return Output(value=df,
                   metadata={
                       "len_df": MetadataValue.int(len(df)),

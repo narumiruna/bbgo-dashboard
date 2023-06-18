7b0203f9f53f464dfaae1946b1c2cfc5d92f4dfc
なるみ <toucans-cutouts0f@icloud.com>
2023-06-19 01:09:50 +0800

    refactor: refactor assets module and definitions list
    - Add `nav_history_detail` asset to `bbgo_dashboard`
    - Rename `trades.py` to `assets.py`
    - Change the return type of `trades` asset to `pd.DataFrame`
    - Add `nav_history_detail` asset to the `Definitions` list
    - Remove `daily_num_trades` asset from `Definitions` list and import it from a separate file
    - Remove `daily_num_trades` import from `assets/__init__.py` and add import for `nav_history_detail` and `trades` assets
    
    Signed-off-by: なるみ <toucans-cutouts0f@icloud.com>

--- bbgo_dashboard/assets/__init__.py
+++ bbgo_dashboard/assets/__init__.py
@@ -1,12 +1,13 @@
 from dagster import Definitions
 from dagster import FilesystemIOManager
 
+from .assets import daily_num_trades
+from .assets import nav_history_detail
+from .assets import trades
 from .resources import DatabaseResource
-from .trades import daily_num_trades
-from .trades import trades
 
 defs = Definitions(
-    assets=[trades, daily_num_trades],
+    assets=[trades, daily_num_trades, nav_history_detail],
     resources={
         'db': DatabaseResource(),
         'io_manager': FilesystemIOManager(base_dir='data'),
--- bbgo_dashboard/assets/assets.py
+++ bbgo_dashboard/assets/assets.py
@@ -4,14 +4,14 @@ from dagster import Output
 from dagster import asset
 from sqlalchemy import select
 
+from ..db import NavHistoryDetail
 from ..db import Trade
 from .resources import DatabaseResource
 
 
 @asset
-def trades(db: DatabaseResource) -> Output:
+def trades(db: DatabaseResource) -> Output[pd.DataFrame]:
     df = db.read_sql(select(Trade), parse_dates=['traded_at'])
-    df.set_index('traded_at', inplace=True)
     return Output(value=df,
                   metadata={
                       "len_df": MetadataValue.int(len(df)),
@@ -20,9 +20,19 @@ def trades(db: DatabaseResource) -> Output:
 
 
 @asset
-def daily_num_trades(trades: pd.DataFrame):
+def daily_num_trades(trades: pd.DataFrame) -> Output[pd.DataFrame]:
     df = trades.groupby([pd.Grouper(freq='D'), 'side']).count()
     df = df[['gid']]
+    return Output(value=df,
+                  metadata={
+                      "len_df": MetadataValue.int(len(df)),
+                      "preview": MetadataValue.md(df.head().to_markdown()),
+                  })
+
+
+@asset
+def nav_history_detail(db: DatabaseResource) -> Output[pd.DataFrame]:
+    df = db.read_sql(select(NavHistoryDetail), parse_dates=['time'])
     return Output(value=df,
                   metadata={
                       "len_df": MetadataValue.int(len(df)),
